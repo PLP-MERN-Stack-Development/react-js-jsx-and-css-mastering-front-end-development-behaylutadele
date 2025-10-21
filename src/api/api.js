29eafc8db07b8e375d4b2ee9d6f3f14058accc26
behaylutadele <104895316+behaylutadele@users.noreply.github.com>
2025-10-21 16:14:11 +0300

    Add fetchPosts function to retrieve posts

---
+++ src/api/api.js
@@ -0,0 +1,5 @@
+export async function fetchPosts(page = 1, limit = 10) {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=${limit}`)
+  if (!response.ok) throw new Error('Failed to fetch data')
+  return response.json()
+}
